94324761c11eb7de79e6707f37e2c32613c942b8
HP EliteBook <HP EliteBook@DESKTOP-QQJ2NTR>
2023-11-13 21:32:26 +0800

    Add project files.

---
+++ 05LaboratoryExercise1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05LaboratoryExercise1
+{
+   public partial class FrmLab1 : Form
+    {
+  
+     
+      public FrmLab1()
+       {
+           
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+       {
+           frmFileName frmFileName = new frmFileName();
+           frmFileName.ShowDialog();
+           frmRegistration frmRegistration = new frmRegistration();
+           frmRegistration.ShowDialog();   
+            
+
+          string getInput = txtInput.Text;
+           string docPath =Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+          using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath,
+          frmFileName.SetFileName)))
+           {
+               outputFile.WriteLine(getInput);
+               Console.WriteLine(getInput);
+          }
+       }
+
+       private void FrmLab1_Load(object sender, EventArgs e)
+       {
+
+     }
+   }
+}
